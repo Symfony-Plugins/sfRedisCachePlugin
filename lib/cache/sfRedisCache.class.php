c860984dfbee0035b13639e914c7fa149c62a7b9
oncletom <oncletom@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-27 16:35:25 -0600

    Completed methods
    git-svn-id: http://svn.symfony-project.com/plugins/sfRedisCachePlugin/trunk@24484 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/cache/sfRedisCache.class.php
+++ lib/cache/sfRedisCache.class.php
@@ -38,15 +38,8 @@ class sfRedisCache extends sfCache
       throw new sfInitializationException('You must have Redis installed as a compiled module or shared class.');
     }
 
-    if ($this->getOption('redis'))
-    {
-      $this->redis = $this->getOption('redis');
-    }
-    else
-    {
-      $this->redis = new Redis;
-      $this->redis->connect($this->getOption('server', '127.0.0.1'), $this->getOption('port', 6379));
-    }
+    $this->redis = $this->getOption('redis') ? $this->getOption('redis') : new Redis;
+    $this->redis->connect($this->getOption('server', '127.0.0.1'), $this->getOption('port', 6379));
 
     if (!$this->redis->ping())
     {
@@ -67,7 +60,9 @@ class sfRedisCache extends sfCache
   */
   public function get($key, $default = null)
   {
-    
+    $value = $this->redis->get($this->getOption('prefix').$key);
+
+    return null === $value ? $default : $value;
   }
 
   /**
@@ -75,7 +70,7 @@ class sfRedisCache extends sfCache
    */
   public function has($key)
   {
-    
+    return $this->redis->exists($this->getOption('prefix').$key);
   }
 
   /**
@@ -83,7 +78,20 @@ class sfRedisCache extends sfCache
    */
   public function set($key, $data, $lifetime = null)
   {
-    
+    $lifetime = null === $lifetime ? $this->getOption('lifetime') : $lifetime;
+
+    if ($lifetime < 1)
+    {
+      $response = $this->remove($key);
+    }
+    else
+    {
+      $this->setMetadata($key, $lifetime);
+      $response = $this->redis->set($this->getOption('prefix').$key, $data, false);
+      $this->redis->expire($this->getOption('prefix').$key, $lifetime);
+    }
+
+    return $response;
   }
 
   /**
@@ -91,7 +99,8 @@ class sfRedisCache extends sfCache
    */
   public function remove($key)
   {
-    
+    $this->deleteMetadata($key);
+    return $this->redis->delete($this->getOption('prefix').$key);
   }
 
   /**
@@ -101,8 +110,21 @@ class sfRedisCache extends sfCache
   {
     if (sfCache::ALL === $mode)
     {
-      
+      $this->removePattern('*');
     }
+  }
+
+  /**
+   * Optimized getMany with Redis mget method
+   * 
+   * @param array $keys
+   * @return array
+   */
+  public function getMany($keys)
+  {
+    $cache_keys = array_map(array($this, 'applyPrefix'),  $keys);
+    
+    return array_combine($keys, $this->redis->mget($cache_keys));
   }
 
   /**
@@ -110,7 +132,19 @@ class sfRedisCache extends sfCache
    */
   public function getLastModified($key)
   {
+    return $this->redis->get($this->getOption('prefix').'lastmodified'.self::SEPARATOR.$key);
+  }
 
+  /**
+   * Checks if a key is expired or not
+   *
+   * @author oncletom
+   * @param string $key
+   * @return boolean
+   */
+  public function isExpired($key)
+  {
+    return time() > $this->getTimeout($key);
   }
 
   /**
@@ -118,19 +152,76 @@ class sfRedisCache extends sfCache
    */
   public function getTimeout($key)
   {
-
+    return $this->redis->get($this->getOption('prefix').'timeout'.self::SEPARATOR.$key);
   }
 
   /**
+   * We manually remove keys as the redis glob style * == sfCache ** style
+   *
+   * @author oncletom
    * @see sfCache
    */
   public function removePattern($pattern)
   {
+    $keys = $this->redis->keys($this->getOption('prefix').$pattern);
 
+    $regexp = self::patternToRegexp($this->getOption('prefix').$pattern);
+    foreach ($keys as $key)
+    {
+      if (preg_match($regexp, $key))
+      {
+        $this->remove(substr($key, strlen($this->getOption('prefix'))));
+      }
+    }
   }
 
-  protected function getCacheInfo()
+  /**
+   * Stores metada for a key
+   *
+   * Why storing timeout despite we have an expire/ttl method? Because the PHP library in 0.1v does not have tll method
+   *
+   * @protected
+   * @author oncletom
+   * @param string $key
+   * @param integer $lifetime
+   * @return string Redis Reply Type
+   */
+  protected function setMetadata($key, $lifetime)
   {
+    $response = $this->redis->set($this->getOption('prefix').'lastmodified'.self::SEPARATOR.$key, time());
+    $this->redis->set($this->getOption('prefix').'timeout'.self::SEPARATOR.$key, time() + $lifetime);
 
+    $this->redis->expire($this->getOption('prefix').'lastmodified'.self::SEPARATOR.$key, $lifetime);
+    $this->redis->expire($this->getOption('prefix').'timeout'.self::SEPARATOR.$key, $lifetime);
+
+    return $response;
+  }
+
+  /**
+   * Deletes every metadata related to a key
+   *
+   * @author oncletom
+   * @param string $key
+   * @return boolean
+   */
+  protected function deleteMetadata($key)
+  {
+    $this->redis->delete($this->getOption('prefix').'lastmodified'.self::SEPARATOR.$key);
+    return !!$this->redis->delete($this->getOption('prefix').'timeout'.self::SEPARATOR.$key);
+  }
+
+  /**
+   * Apply prefix to a value
+   *
+   * Usefull to be mapped on an array. Faster than foreach
+   *
+   * @protected
+   * @author oncletom
+   * @param string $value
+   * @return string
+   */
+  protected function applyPrefix($value)
+  {
+    return $this->getOption('prefix').$value;
   }
 }
